0f96b0d488a0fc191e95f2db0537217eaacf7063
Ozymandias42 <664534+Ozymandias42@users.noreply.github.com>
2018-09-06 16:31:58 +0200

    lots of changes - changed expected predicate return value for bool true from 0 to 1 - added pointer-referential zipWith     relies on a global variable to write into.     this allows "return" of lists of arbitrary content with their field-boundaries intact. I.e.: array length is as expected.     using echo "${out[@]}" breaks those boundaries if fields contian white-space-separated strings. - decoupled zipWith() from zip()

--- functional.sh
+++ functional.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 filter() {
+    func=$1
     arr=(${@:2})
     ret=()
     inc=1
-    for i in ${arr[@]}; do
-        [[ $($1 $i) -eq 0 ]] && { ret[$inc]=$i ; inc=$(($inc+1)) ; } 
+    for ((i=0;i<${#arr[@]};i++)); do
+        [[ $($func "${arr[$i]}") -eq 1 ]] && { ret[$inc]="${arr[$i]}" ; inc=$(($inc+1)) ; }
     done
     echo ${ret[@]}
 }
@@ -15,14 +16,29 @@ filter() {
 #Breaks functional purity through use of references though
 #Needs to be called: filter funcName arrName[@]
 #Other function is called filter funcName "${arrName[@]}"
-filter() {
+reffilter() {
+    func=$1
     arr=("${!2}")
     ret=()
     inc=1
-    for i in ${arr[@]}; do
-        [[ $($1 $i) -eq 0 ]] && { ret[$inc]=$i ; inc=$(($inc+1)) ; }
+    for ((i=0;i<${#arr[@]};i++)); do
+        [[ $($func "${arr[$i]}") -eq 1 ]] && { ret[$inc]="${arr[$i]}" ; inc=$(($inc+1)) ; }
     done
     echo ${ret[@]}
+}
+
+preffilter() { 
+#Variant w/o echo-return. needed when filtering space separated strings.
+#allows to "return" arrays with arbitrary content. No accidential separation via spaces possible.
+#needs to be passed the name of a global res array.
+#call like this: preffilter predicate arr[@] res
+    func=$1
+    arr=("${!2}")
+    local -n ret=${3}
+    inc=1
+    for ((i=0;i<${#arr[@]};i++)); do
+        [[ $($func "${arr[$i]}") -eq 1 ]] && { ret[$inc]="${arr[$i]}" ; inc=$(($inc+1)) ; }
+    done
 }
 
 #Usage refmap callback arr[@] => []
@@ -93,21 +109,55 @@ unfold() {
     echo ${out[@]}
 }
 
+refzip() {
+    arr1=( "${!1}" )
+    arr2=( "${!2}" )
+    local -n refzip_out=${3}
+    
+    [[ ${#arr1[@]} -ge ${#arr2[@]} ]] && use=2 || use=1
+    [[ ${#arr1[@]} -eq ${#arr2[@]} ]] && use=1
+
+    index=0
+    sep="${4:-" "}" #separator for resulting tuples. $3 if set. else :
+    while [[ $index -le $( [[ $use -eq 1 ]] && echo ${#arr1[@]} || echo ${#arr2[@]} ) ]] ; do
+        refzip_out[$index]=\'${arr1[$index]}$sep${arr2[$index]}\'
+        index=$(($index+1))
+    done
+    echo ${zip_out[@]}
+}
+
 zip() {
     arr1=( "${!1}" )
     arr2=( "${!2}" )
-    out=()
+    zip_out=()
     
     [[ ${#arr1[@]} -ge ${#arr2[@]} ]] && use=2 || use=1
     [[ ${#arr1[@]} -eq ${#arr2[@]} ]] && use=1
 
     index=0
     sep="${3:-" "}" #separator for resulting tuples. $3 if set. else :
-    while [[ $index -ne $( [[ $use -eq 1 ]] && echo ${#arr1[@]} || echo ${#arr2[@]} ) ]] ; do
-        out=( ${out[@]} \'${arr1[$index]}$sep${arr2[$index]}\' )
+    while [[ $index -le $( [[ $use -eq 1 ]] && echo ${#arr1[@]} || echo ${#arr2[@]} ) ]] ; do
+        zip_out[$index]=\'${arr1[$index]}$sep${arr2[$index]}\'
         index=$(($index+1))
     done
-    echo "${out[@]}"
+    echo ${zip_out[@]}
+}
+
+prefzipWith() {
+    func=${1}
+    arr1=( "${!2}" )
+    arr2=( "${!3}" )
+    additionalParameters=( "${!5}" )
+    local -n refzipWith_out=${4} #pointer to external result array. allows for true return of arrays
+
+    [[ ${#arr1[@]} -ge ${#arr2[@]} ]] && use=2 || use=1
+    [[ ${#arr1[@]} -eq ${#arr2[@]} ]] && use=1
+
+    index=0
+    while [[ $index -le $( [[ $use -eq 1 ]] && echo ${#arr1[@]} || echo ${#arr2[@]} ) ]] ; do
+        refzipWith_out[$index]=$( $func "${arr1[$index]}" "${arr2[$index]}" "${additionalParameters[@]}" )
+        index=$(($index+1))
+    done
 }
 
 zipWith() {
@@ -115,12 +165,14 @@ zipWith() {
     arr1=( "${!2}" )
     arr2=( "${!3}" )
     tuplesep="${4:-":"}"
-    tmpres=$(zip arr1[@] arr2[@] $tuplesep)
-    res=()
-    for i in ${tmpres[@]}; do
-        arg1=$(echo $i|cut -d "'" -f 2|cut -d"$tuplesep" -f 1)
-        arg2=$(echo $i|cut -d"$tuplesep" -f 2|cut -d"'" -f 1)
-        res=( ${res[@]} $($func $arg1 $arg2) )
+    tmpres=(zip arr1[@] arr2[@] $tuplesep)
+    zipWith_out=()
+    i=0
+     while [[ $i -le ${#tmpres[@]} ]]; do
+        arg1=$(echo ${tmpres[$i]}|cut -d "'" -f 2|cut -d"$tuplesep" -f 1)
+        arg2=$(echo ${tempres[$i]}|cut -d"$tuplesep" -f 2|cut -d"'" -f 1)
+        zipWith_out[$index]=$($func $arg1 $arg2)
+        index=$(($index+1))
     done
     echo ${res[@]}
 }
@@ -134,9 +186,10 @@ forEachParallel() {
 }
 
 forEach() {
-    arr=(${@:2})
-    for i in ${arr[@]}; do
-        $1 $i
+    func=${1}
+    arr=( "${!2}" )
+    for ((i=0;i<${#arr[@]};i++));do
+        $func "${arr[$i]}" ${!3:@}
     done
 }
 
